28c1523be178288421230ea6b6762b7b66456f7c
lfbianchi <lfbianchi3632@gmail.com>
2021-06-08 10:57:53 +0100

    fixed exposed key

--- framestore/settings.py
+++ framestore/settings.py
@@ -1,4 +1,4 @@
-import os
+import os, json
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -8,8 +8,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
+with open("static/credentials.json", "r") as credentials:
+    tokens = json.load(credentials)
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^(_51&0^&fo*8$dnqg_8vb_dr5cv0j-@%xlw@8moemwi9p5rw('
+SECRET_KEY = tokens['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
